ac4f08937d21dfeaae635afef05ae374ec9f91be
siddhu-17 <127585424+siddhu-17@users.noreply.github.com>
2025-07-06 11:00:20 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://greatstack:5316@cluster0.erikw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-   
+    await mongoose.connect('mongodb+srv://greatstack:5316@cluster0.erikw.mongodb.net/food?retryWrites=true&w=majority');
     console.log("DB Connected");
   } catch (error) {
     console.error("Connection failed:", error.message);
